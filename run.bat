e0cf7aa7c3edb2fc8edfd47115603fde14e88c06
3143786339 <3143786339@qq.com>
2022-01-25 14:47:20 +0800

    1.25-Linux试运行

--- run.bat
+++ run.bat
@@ -1,2 +1,6 @@
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.1-39.0.58/win_args.txt %*
+REM Forge requires a configured set of both JVM and program arguments.
+REM Add custom JVM arguments to the user_jvm_args.txt
+REM Add custom program arguments {such as nogui} to this file in the next line before the %* or
+REM  pass them to this script directly
+java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.1-39.0.59/win_args.txt %*
 pause
